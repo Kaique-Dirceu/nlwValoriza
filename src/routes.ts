e32b77bc056a606f524a00c2529e80e29b13b155
Kaique-Dirceu <dirceukaique@gmail.com>
2021-12-16 10:10:51 -0300

    feat: create tags

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
+import { ensureAdim } from './middlewares/ensureAdmin';
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handle)
+router.post("/tags", ensureAdim, createTagController.handle)
 
 export { router }
